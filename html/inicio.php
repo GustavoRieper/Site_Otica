56ac2e77f367956325254124b14182ecbf9a9473
Gustavo Rieper <gustavorieper@gmail.com>
2018-05-05 21:36:18 -0300

    Versão Única
    Desenvolvido utilizando linguagem PHP apropriada para servidores. Pode ser utilizada em sessões modificando para postagem de produto.
    
    Na publicação está apenas a parte visual. Use sua imaginação e modifique ou adapte avontade...

---
+++ html/inicio.php
@@ -0,0 +1,105 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="../css/inicio.css"/>
+        <link rel="stylesheet" type="text/css" href="../css/padroes/reset.css"/>
+        <title>Ótica - Brasil</title>
+    </head>
+
+    <body>
+        <?php include("padroes/sessao1.php"); ?><!-- Import sessao1 -->
+        <section class="sessao2">
+            <img id="banner-section2" src="../imagens/inicio/banner_oculos_garota.png">
+            <p>Presente em <br>todos os estados<br> brasileiros<br>
+            <img id="img_estados" src="../imagens/inicio/estados_brasil.png"></p>            
+        </section>
+        <section class="sessao3">
+            <ul>
+                <li><h2>Rosto Oval</h2>
+                    <ul>
+                        <li><a href=""  title="Armani Collezioni AC951"><img id="ico_estilos" src="../imagens/inicio/rosto_oval_01.png">AC951<img id="ico_marcas" src="../imagens/inicio/marca_armani_collezioni.png" title="Armani Collezioni"> </a></li>
+                        <li><a href="" title="Baccarat BC632"><img id="ico_estilos" src="../imagens/inicio/rosto_oval_02.png">BC632<img id="ico_marcas" src="../imagens/inicio/marca_baccarat.png" title="Baccarat">  </a></li>
+                        <li><a href="" title="Armani Collezioni AC951"><img id="ico_estilos" src="../imagens/inicio/rosto_oval_03.png">HB4171 <img id="ico_marcas" src="../imagens/inicio/marca_boos_hugo_boss.png" title="Hugo Boss"></a></li>
+                        <li><a href="" title="Armani Collezioni AC951"><img id="ico_estilos" src="../imagens/inicio/rosto_oval_04.png">CN5674 <img id="ico_marcas" src="../imagens/inicio/marca_canali.png" title="Canali"></a></li>
+                    </ul>
+                </li>
+                <li><h2>Rosto Redondo</h2>
+                    <ul>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_redondo_01.png">CL321<img id="ico_marcas" src="../imagens/inicio/marca_chanel.png" title="Chanel"></a></li>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_redondo_02.png">DG532<img id="ico_marcas" src="../imagens/inicio/marca_d_e_g.png" title="D&G"></a></li>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_redondo_03.png">FR441<img id="ico_marcas" src="../imagens/inicio/marca_ferrari_store.png" title="Ferrari Store"></a></li>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_redondo_04.png">GX611<img id="ico_marcas" src="../imagens/inicio/marca_geox.png" title="GEOX"></a></li>
+                    </ul>
+                </li>
+                <li><h2>Rosto Coração</h2>
+                    <ul>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_coracao_01.png">GA711<img id="ico_marcas" src="../imagens/inicio/marca_giorgio_armani.png" title="Giorgio Armani"></a></li>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_coracao_02.png">NK231<img id="ico_marcas" src="../imagens/inicio/marca_ray_ban.png" title="Ray Ban"></a></li>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_coracao_03.png">RB741<img id="ico_marcas" src="../imagens/inicio/marca_nike.png" title="Nike"></a></li>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_coracao_04.png">RR621<img id="ico_marcas" src="../imagens/inicio/marca_rock_e_republic.png" title="Rock & Republic"></a></a></li>
+                    </ul>
+                </li>
+                <li><h2>Rosto Quadrado</h2>
+                    <ul>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_quadrado_01.png">HG114<img id="ico_marcas" src="../imagens/inicio/marca_boos_hugo_boss.png" title="Hugo Boss"></a></li>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_quadrado_02.png">DG976<img id="ico_marcas" src="../imagens/inicio/marca_d_e_g.png" title="D&G"></a></li>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_quadrado_03.png">GX341<img id="ico_marcas" src="../imagens/inicio/marca_geox.png" title="GEOX"></a></li>
+                        <li><a href=""><img id="ico_estilos" src="../imagens/inicio/rosto_quadrado_04.png">GA432<img id="ico_marcas" src="../imagens/inicio/marca_giorgio_armani.png" title="Giorgio Arnani"></a></li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+        <section class="sessao4">
+            <h2><a name="marcas">Marcas</a></h2>
+            <div class="marcas">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_armani_collezioni.png" title="Armani Collezioni">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_baccarat.png" title="Baccarat">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_boos_hugo_boss.png" title="Hugo Boss">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_canali.png" title="Canali">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_chanel.png" title="Chanel">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_d_e_g.png" title="D&G">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_ferrari_store.png" title="Ferrari Store">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_geox.png" title="GEOX">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_giorgio_armani.png" title="Giorgio Armani">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_nike.png" title="Nike">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_ray_ban.png" title="Ray Ban">
+                <img id="img_marcas" src="../imagens/inicio/marcas_black/marca_rock_e_republic.png" title="Rock & Recuplic">
+            </div>
+        </section>
+        <section class="sessao5">
+            <h2><a name="contato">Contato</a></h2>
+            <form>
+                <input type="text" value="" name="nm_usuario" placeholder="Insira seu Nome Completo" title="Insira seu Nome Completo">
+                <br>                
+                <input type="email" value="" name="email"placeholder="Insira seu E-mail" title="Insira seu E-mail">
+                <br>
+                <h3  title="Receber notícias e ofertas"><input type="checkbox" value="";>Receber notícias e ofertas</h3>
+                <br>
+                <textarea name="mensagem" rows="10" cols="50" placeholder="Insira o conteúdo." title="Insira o conteúdo.">
+                </textarea>
+                <br>
+                <input type="submit" name="enviar" title="enviar">
+            </form>
+            <h3>Ótica Brasil</h3>
+            <p id="cont_p1">Rua Inácio de Oliveira<br>Nº 350 / Itaum</p>
+            <p id="cont_p2">Joinville/SC</p>
+            <p id="cont_p3">Cep - 89210-128</p>            
+        </section>  
+        <iframe id="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3575.8667640653066!2d-48.835476885415325!3d-26.330813475526295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94deb11941a4d88b%3A0xc7f649c6bdff72c0!2sRua+In%C3%A1cio+de+Oliveira%2C+350+-+Itaum%2C+Joinville+-+SC!5e0!3m2!1spt-BR!2sbr!4v1510050539781" frameborder="0" allowfullscreen></iframe>
+        <footer>
+            <br>
+            <h3>Siga nossas redes sociais</h3>
+            <div id="redes_sociais">
+                <a href="#"><img src="../imagens/inicio/logo_facebook.png"></a>
+                <a href="#"><img src="../imagens/inicio/logo_insta.png"></a>
+                <a href="#"><img src="../imagens/inicio/logo_linke.png"></a>
+                <a href="#"><img src="../imagens/inicio/logo_twitter.png"></a>
+            </div>           
+            <p> Todos os Direitos Reservados &copy;</p>            
+        </footer>        
+    </body>
+</html>
---
+++ html/padroes/sessao1.php
@@ -0,0 +1,43 @@
+<?php
+?>
+<head>
+        <link rel="stylesheet" type="text/css" href="../css/padroes/reset.css"/>
+        <style>
+            .menu{
+                position: relative;
+                float: right;
+                margin-right: 50px;
+            }
+            .menu li{
+                font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+                display: inline;
+                text-transform: uppercase;
+                font-size:14pt;
+                transition: all 0.2s;
+            }
+            .menu li a{
+                text-decoration:none;
+                color: #b60202;
+            }
+            .menu li a:hover{
+                color:#e95050;
+                transition: all 0.2s;
+            }
+        </style>
+</head>
+<section class="sessao1">
+    <h1> A maior rede de ótica do brasil </h1>
+    <div id="back_section1">
+        <a href="http://localhost/otica/html/inicio.php">        
+            <img id="logo_sessao1" src="../imagens/padroes/logo.png" title="Frederique Constant" />
+        </a>
+        <nav>
+            <ul class="menu">
+                <li><a href="#">Inicio</a> | </li>
+                <li><a href="#marcas">Marcas</a> | </li>
+                <li><a href="#">Parceiros</a> | </li>
+                <li><a href="#contato">Contato</a></li>
+            </ul>
+        </nav>
+    </div>
+</section>
